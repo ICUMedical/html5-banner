19da872f54c9cf5e1b66b5bbce0e4b2bebc35186
ICUMedical <50720251+ICUMedical@users.noreply.github.com>
2019-05-16 14:40:35 -0700

    Delete index.js

--- index.js
+++
@@ -1,5 +0,0 @@
-function usBanner() {
-  $('#image1').delay(3000).fadeOut(600).delay(3000).fadeIn(600);
-  $('#image2').delay(3000).fadeIn(600).delay(3000).fadeOut(600);
-};
-setInterval("usBanner()", 100);
